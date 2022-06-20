2a2048ed08674fbd3c8ad73cb30104d987af9965
Ivan Borisov <ivan.alo@mail.ru>
2022-06-21 00:35:51 +0300

    Add MoexClient

---
+++ internal/api/moexclient.go
@@ -0,0 +1,163 @@
+package api
+
+import (
+	"bonds_calculator/internal/model/moex"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
+
+const (
+	allBondsUrl = "https://iss.moex.com/iss/engines/stock/markets/bonds/securities.csv?iss.meta=off&iss.only=marketdata,securities&securities.columns=SECID,SHORTNAME,COUPONVALUE,NEXTCOUPON,ACCRUEDINT,PREVPRICE,FACEVALUE,COUPONPERIOD,MINSTEP,COUPONPERCENT&marketdata.columns=SECID,LCURRENTPRICE"
+
+	allBondizationUrl = "https://iss.moex.com/iss/securities/${bond}/bondization.csv?limit=unlimited&iss.meta=off&iss.only=amortizations,coupons&amortizations.columns=amortdate,value&coupons.columns=coupondate,value"
+)
+
+type MoexClient struct {
+	innerClient http.Client
+
+	workQueue chan worker
+
+	isClosedChan chan struct{}
+}
+
+func NewMoexClient(queueSize int) MoexClient {
+	client := MoexClient{
+		workQueue:    make(chan worker, queueSize),
+		isClosedChan: make(chan struct{}),
+	}
+
+	go queueListener(client)
+
+	return client
+}
+
+func (client *MoexClient) Close() error {
+	close(client.isClosedChan)
+
+	return nil
+}
+
+func (client *MoexClient) GetBonds() ([]moex.Bond, error) {
+	request := bondsRequest{newCommonRequest[[]moex.Bond]()}
+
+	return request.commonReturn(client, &request)
+}
+
+func (client *MoexClient) GetBondization(Id string) (moex.Bondization, error) {
+	request := bondizationRequest{
+		bondId:        Id,
+		commonRequest: newCommonRequest[moex.Bondization](),
+	}
+
+	return request.commonReturn(client, &request)
+}
+
+type worker interface {
+	work(client MoexClient)
+}
+
+func queueListener(client MoexClient) {
+	for {
+		select {
+		case worker := <-client.workQueue:
+			worker.work(client)
+		case <-client.isClosedChan:
+			return
+		}
+	}
+}
+
+type commonRequest[T any] struct {
+	result   T
+	err      error
+	doneChan chan struct{}
+}
+
+func newCommonRequest[T any]() commonRequest[T] {
+	return commonRequest[T]{
+		doneChan: make(chan struct{}),
+	}
+}
+
+func (request *commonRequest[R]) commonReturn(client *MoexClient, worker worker) (R, error) {
+	client.workQueue <- worker
+
+	<-request.doneChan
+
+	return request.result, request.err
+}
+
+type bondsRequest struct {
+	commonRequest[[]moex.Bond]
+}
+
+func (bondsRequest *bondsRequest) work(client MoexClient) {
+	defer close(bondsRequest.doneChan)
+
+	resp, err := client.innerClient.Get(allBondsUrl)
+
+	if resp != nil {
+		defer resp.Body.Close()
+	}
+
+	if err != nil {
+		bondsRequest.err = err
+
+		return
+	}
+
+	buf, err := io.ReadAll(resp.Body)
+	if err != nil {
+		bondsRequest.err = fmt.Errorf("cannot read body %v", err)
+
+		return
+	}
+
+	bonds, err := moex.ParseBondsCp1251(buf)
+	if err != nil {
+		bondsRequest.err = err
+
+		return
+	}
+
+	bondsRequest.result = bonds
+}
+
+type bondizationRequest struct {
+	bondId string
+	commonRequest[moex.Bondization]
+}
+
+func (bondizationRequest *bondizationRequest) work(client MoexClient) {
+	defer close(bondizationRequest.doneChan)
+
+	resp, err := client.innerClient.Get(strings.Replace(allBondizationUrl, "${bond}", bondizationRequest.bondId, 1))
+
+	if resp != nil {
+		defer resp.Body.Close()
+	}
+
+	if err != nil {
+		bondizationRequest.err = err
+
+		return
+	}
+
+	buf, err := io.ReadAll(resp.Body)
+	if err != nil {
+		bondizationRequest.err = fmt.Errorf("cannot read body %v", err)
+
+		return
+	}
+
+	bondization, err := moex.ParseBondization(bondizationRequest.bondId, buf)
+	if err != nil {
+		bondizationRequest.err = err
+
+		return
+	}
+
+	bondizationRequest.result = bondization
+}
--- test/model/moex/integration_test.go
+++ test/model/moex/integration_test.go
@@ -1,35 +1,22 @@
 package moex
 
 import (
-	"bonds_calculator/internal/model/moex"
+	"bonds_calculator/internal/api"
 	asserts "github.com/stretchr/testify/assert"
-	"io"
-	"strings"
 	"sync"
 	"testing"
-)
-
-var (
-	allBondsUrl       = "https://iss.moex.com/iss/engines/stock/markets/bonds/securities.csv?iss.meta=off&iss.only=marketdata,securities&securities.columns=SECID,SHORTNAME,COUPONVALUE,NEXTCOUPON,ACCRUEDINT,PREVPRICE,FACEVALUE,COUPONPERIOD,MINSTEP,COUPONPERCENT&marketdata.columns=SECID,LCURRENTPRICE"
-	allBondizationUrl = "https://iss.moex.com/iss/securities/${bond}/bondization.csv?limit=unlimited&iss.meta=off&iss.only=amortizations,coupons&amortizations.columns=amortdate,value&coupons.columns=coupondate,value"
 )
 
 func TestBondNullFields(t *testing.T) {
 	t.Parallel()
 
 	assert := asserts.New(t)
-	resp, err := client.Get(allBondsUrl)
-	assert.NoError(err, "getting all bonds")
 
-	if resp.Body != nil {
-		defer resp.Body.Close()
-	}
+	client := api.NewMoexClient(1)
+	defer client.Close()
 
-	buf, err := io.ReadAll(resp.Body)
-	assert.NoError(err, "reading body")
-
-	bonds, err := moex.ParseBondsCp1251(buf)
-	assert.NoError(err, "unmarshalling bonds")
+	bonds, err := client.GetBonds()
+	assert.NoError(err, "getting bonds")
 
 	for _, bond := range bonds {
 		assert.NotEqual(0, bond.Coupon)
@@ -47,51 +34,26 @@ func TestLoadAllBondizations(t *testing.T) {
 
 	assert := asserts.New(t)
 
-	resp, _ := client.Get(allBondsUrl)
-	if resp.Body != nil {
-		defer resp.Body.Close()
-	}
+	client := api.NewMoexClient(50)
+	defer client.Close()
 
-	buf, _ := io.ReadAll(resp.Body)
-
-	bonds, _ := moex.ParseBondsCp1251(buf)
+	bonds, _ := client.GetBonds()
 
 	wg := sync.WaitGroup{}
+	wg.Add(len(bonds))
 
-	requestChan := make(chan string)
+	for _, bond := range bonds {
+		go func(id string) {
+			bondization, err := client.GetBondization(id)
+			assert.NoError(err, "getting bondization")
 
-	for i := 0; i < 10; i++ {
-		wg.Add(1)
-
-		go func() {
-			for id := range requestChan {
-				resp, err := client.Get(strings.ReplaceAll(allBondizationUrl, "${bond}", id))
-				assert.NoError(err, "getting all bondizations")
-
-				if resp.Body != nil {
-					defer resp.Body.Close()
-				}
-
-				buf, err := io.ReadAll(resp.Body)
-				assert.NoError(err, "read bondization body")
-
-				bondization, err := moex.ParseBondization(id, buf)
-				assert.NoError(err, "unmarshalling bondization")
-
-				for _, amortization := range bondization.Amortizations {
-					assert.NotEqual(0, amortization.Value)
-				}
+			for _, amortization := range bondization.Amortizations {
+				assert.NotEqual(0, amortization.Value)
 			}
 
 			wg.Done()
-		}()
+		}(bond.Id)
 	}
-
-	for _, bond := range bonds {
-		requestChan <- bond.Id
-	}
-
-	close(requestChan)
 
 	wg.Wait()
 }
